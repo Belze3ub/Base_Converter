b2c4a596a9653579160e39b4171e40b208a10bde
Mikolaj Gawlik <mikolaj3313@gmail.com>
2023-06-22 19:46:30 +0200

    Removed color property

--- bin2dec/src/Button.tsx
+++ bin2dec/src/Button.tsx
@@ -1,11 +1,10 @@
 interface Props {
-  color: string;
   children: string;
   className: string;
   onClick?: () => void;
 }
 
-const Button = ({color, children, className, onClick}: Props) => {
-  return <button className={className} style={{backgroundColor: color}} color={color} onClick={onClick}>{children}</button>;
+const Button = ({children, className, onClick}: Props) => {
+  return <button className={className} onClick={onClick}>{children}</button>;
 }
 export default Button
